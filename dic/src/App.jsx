8b49348fecaf833c590214e52dcc34b4d42023d8
Julian <julian.thoms@bertrandt.com>
2021-12-15 23:17:36 +0100

    added analytics monka

--- dic/src/App.jsx
+++ dic/src/App.jsx
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 import Inventory from './inventory/Inventory.jsx'
+import ReactGA from "react-ga4";
 
 function App() {
+  ReactGA.initialize('G-R9K7DPFTRE')
+  ReactGA.send('pageview')
   return (
     <div className="App">
-      <Inventory/>
+      <Inventory />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
